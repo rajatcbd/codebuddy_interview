a8dfa1a7bd3bc37185b6f61b35eb9536bcd3de95
hyvip-ai <rm2932002@gmail.com>
2022-01-11 16:39:03 +0530

    Initial Commit

--- dslevelx.js
+++ dslevelx.js
@@ -1,20 +1,29 @@
-let arr = [1, 3, 4, 6, 7, 8]
-
-let myArr = arr.filter(item=>{
-    return item%2
-})
-console.log(myArr)
-
-let myStr = "Elie"
-let vowelArr = ['a','e','i','o','u']
-for(let item of myStr){
-    if(vowelArr.includes(item.toLocaleLowerCase())){
-        myStr = myStr.replace(item,item.toUpperCase())
-    }
-    console.log(myStr)
-
+function removeEven(arr) {
+  let myArr = arr.filter((item) => {
+    return item % 2;
+  });
+  return myArr;
 }
-
-let maxArr = [1, 3, 4, 6, 7, 8, 2, 5]
-let max= Math.max(...maxArr)
-console.log(max)
+let arr = [1, 3, 4, 6, 7, 8];
+let data = removeEven(arr);
+console.log(data);
+let myStr = "Elie";
+function makeCaps(myStr) {
+  let vowelArr = ["a", "e", "i", "o", "u"];
+  for (let item of myStr) {
+    if (vowelArr.includes(item.toLocaleLowerCase())) {
+      myStr = myStr.replace(item, item.toUpperCase());
+    }
+   
+  }
+}
+let caps = makeCaps(myStr)
+console.log(myStr)
+let maxArr = [1, 3, 4, 6, 7, 8, 2, 5];
+function maxInArr(maxArr){
+    
+let max = Math.max(...maxArr);
+return max
+}
+let max = maxInArr(maxArr)
+console.log(max);
