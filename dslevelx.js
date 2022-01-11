985fc8e79a600857e6473aec4ef7e580b3d03797
hyvip-ai <rm2932002@gmail.com>
2022-01-11 17:41:24 +0530

    Initial commit

--- dslevelx.js
+++ dslevelx.js
@@ -16,9 +16,10 @@ function makeCaps(myStr) {
     }
    
   }
+  return myStr
 }
 let caps = makeCaps(myStr)
-console.log(myStr)
+console.log(caps)
 let maxArr = [1, 3, 4, 6, 7, 8, 2, 5];
 function maxInArr(maxArr){
     
